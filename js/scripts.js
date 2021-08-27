8cba2697b2294ac4df42e87acec7732d643ca278
JustinasGaripovas <justinas.garipovas@gmail.com>
2021-08-28 00:26:19 +0300

    d
    d

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+/*!
+* Start Bootstrap - Landing Page v6.0.3 (https://startbootstrap.com/theme/landing-page)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-landing-page/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+$( document ).ready(function() {
+
+    console.log("Hey");
+
+    $('.carousel-control-prev').click(() => {
+        $('#carousel').carousel('prev')
+    })
+
+    $('.carousel-control-next').click(() => {
+        $('#carousel').carousel('next')
+    })
+});
